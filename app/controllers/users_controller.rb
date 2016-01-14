e5b084eeb75e9ac4088e6da0577818a5bd96c3c6
dawnfh <2014fed@gmail.com>
2016-01-14 01:42:58 -0500

    practice upload rails

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+  	@users = User.all
+  end
+
+  def show
+  	@user = User.find(params[:fname])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+ 			@user = User.new(params[:user])
+ 		if @user.save
+
+	 		flash[:notice] = "Your account was created
+			successfully."
+			# redirect_to user_path @user
+			redirect_to users_path
+ 		else
+		 	flash[:alert] = "There was a problem saving your
+		 	account."
+		 	redirect_to new_user_path
+ 		end
+	end
+
+	def show
+		@user = User.find(params [:id])
+	end
+end
